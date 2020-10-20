2a11c2d376874465ff7b42b789abf025541f7621
David Lorenz <info@activenode.de>
2020-10-20 14:09:42 +0200

    Added button more dynamic to be able to use button groups
    A button can now have its shadow and its borders removed
    
    Added explicit ViewEncapsulation value to have more clarity

--- design-system/projects/pattern-lib/src/lib/button/button.component.ts
+++ design-system/projects/pattern-lib/src/lib/button/button.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'pl-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss']
+  styleUrls: ['./button.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class ButtonComponent implements OnInit {
   @Input('label') label: string | null;
   @Input('pink') pink: boolean;
+  @Input('noShadow') noShadow: boolean;
+  @Input('bordersLeft') bordersLeft: boolean = true;
+  @Input('bordersRight') bordersRight: boolean = true;
 
   constructor() { }
 
