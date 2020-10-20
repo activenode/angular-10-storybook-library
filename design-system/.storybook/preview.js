c2cef31effd697b6833261cae48c07cd5ad2872e
David Lorenz <info@activenode.de>
2020-10-20 14:01:26 +0200

    added global scss file to storybook

--- design-system/.storybook/preview.js
+++ design-system/.storybook/preview.js
@@ -1,4 +1,5 @@
 
+import '!style-loader!css-loader!sass-loader!./../projects/pattern-lib/src/styles/global.scss';
 import { setCompodocJson } from "@storybook/addon-docs/angular";
 import docJson from "../documentation.json";
 setCompodocJson(docJson);
